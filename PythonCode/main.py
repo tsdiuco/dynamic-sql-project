917f2a15f91756794b312a299b392254537cd73c
Trevor Diuco <tdiuco@ada.gonzaga.edu>
2021-12-15 19:37:48 -0800

    commit

--- PythonCode/main.py
+++ PythonCode/main.py
@@ -1,5 +1,4 @@
 import config
-from finalProject import *
 import mysql.connector
 
 def main():
@@ -17,24 +16,84 @@ def main():
                                     database = db)
 
       # Main Functionality
-      welcomeSelection = None
-      printWecomePrompt()
-      while welcomeSelection != 0 or welcomeSelection != 1:
-         welcomeSelection = input("Enter your selection (1 or 2): ")
-         if (welcomeSelection == 1)
-            printOLTPTransactions()
-         elif (welcomeSelection == 2):
-            printOLAPTransations()
-         else:
-            print("Invalid Input...")
-            printWelcomePrompt()
-
-      
+      print("*** Welcome to Cocktail Creator ***")
+      username = input("Please Enter Your User Name: ")
+      if confirmUserName(username, con):
+         print("Thank You")
+      else:
+         print("Not a username")
 
       con.close()
 
    except mysql.connector.Error as err:
       print(err)
+
+# # OLTP Functionality
+
+# def addUser():
+
+# def removeUser():
+
+# def addReview():
+
+# def listIngredientsOnHand():
+
+# def editOnHandIngredientsList():
+
+# def editOnHandSpiritsList():
+
+# def requestRecipe():
+
+# def increseRecipePortion():
+
+# def findHighestRatedRecipe():
+
+# # OLAP Functionality
+
+# def findAccessableRecipies():
+
+# def filterCocktailRecipies():
+
+# def returnMostUsedIngredients():
+
+# def returnMostUsedSpirits():
+
+# def returnMostUsedIngredients(favSpirit):
+
+# def returnMostUsedIngredients(favSpirit):
+
+# Other Functions
+
+def confirmUserName(username_input, con):
+   rs = con.cursor(buffered=True)
+   query = """ SELECT username
+               FROM application_user;"""
+   rs.execute(query)
+   for (username) in rs:
+      if username == username_input:
+         rs.close()
+         return True
+   rs.close()
+   return False
+
+def printWelcomePrompt():
+   print("***OLTP Transactions***")
+   print("1. Add a User")
+   print("2. Remove a User")
+   print("3. Add a Review")
+   print("4. List Ingredients You Have")
+   print("5. Edit Spirits You Have")
+   print("6. Edit Ingredients You Have")
+   print("7. Search For a Recipe")
+   print("8. Make Multiple Portions of a Recipe")
+   print("9. Find Highest Rated Recipe\n")
+   print("***OLAP Transactions***")
+   print("10. Find Recipes Based on What You Have")
+   print("11. Find Recipes by Filter")
+   print("12. Find Most Common Add Ons")
+   print("13. Find Most Common Spirits")
+   print("14. Find Most Common Add OnsFrom Your Favorite Spirit")
+   print("15. Find Most Common Spirits From Your Favorite Spirit")
 
 if __name__ == '__main__':
    main()
